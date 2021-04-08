d7495c8fa15715d7ebf06f80f69ebf4a8c6e73b7
Laura DiSanza <ldisanza7@gmail.com>
2021-04-08 16:15:57 -0500

    JavaScript addition

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/* From https://www.solodev.com/blog/web-design/how-to-design-a-testimonial-slider.stml 
+
+$(document).ready(function() {
+    $(".testimonial-carousel").slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: false,
+      arrows: true,
+      prevArrow: $(".testimonial-carousel-controls .prev"),
+      nextArrow: $(".testimonial-carousel-controls .next")
+    });
+  });
+
+
+*/
