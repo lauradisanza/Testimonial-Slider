76e3ae4bcba116be2d0881ebd0d9aba0d6359570
Laura DiSanza <ldisanza7@gmail.com>
2021-04-19 20:49:09 -0500

    Javascript added

--- js/script.js
+++ js/script.js
@@ -1,60 +1,38 @@
-/* 33 minutes https://www.youtube.com/watch?v=fZlPJxq0kbk */
 const next = document.querySelector(".next");
-const prev = document.querySelector(".previous");
-const slide = document.querySelector(".testimonial");
+const prev = document.querySelector(".prev");
+const slides = document.querySelectorAll(".slide");
 
-function nextSlide() {
-  slides.forEach((slide) => {
+let index = 0;
+display(index);
+
+function display(index) {
+  slides.forEach(slide=>{
     slide.style.display = 'none';
   });
+  slides[index].style.display = "flex";
 }
 
-
-/*  // Grabbing the whole testimonial sliders
-const testimonialSlider = document.querySelector(".testimonials-container");
-
-if (testimonialSlider != null) { //don't want to run this script if the slideshow is not present
-  let slides = document.querySelector(".testimonial"), //grab all slides
-  slideCount = slides.clientHeight, //count slides (2)
-  currentSlide = 0, //give a starting place
-  slideHeight = null,
-
-  slides[0].classList.add(".active"); //on load, activate the first slide
-
-  function moveToSlide(n) {
-    slides[currentSlide].className = "testimonial";
-    currentSlide = (n+slideCount)%slideCount;
-    slides[currentSlide].className = "testimonial active";
+function nextSlide() {
+  index++;
+  if(index > slides.length-1) {
+    index = 0;
   }
-};
-
-function nextSlide(e){
-  moveToSlide(currentSlide+1);
-  let code = e.keyCode;
-  if( code == 39 ) { //right arrow key
-    moveToSlide(currentSlide+1);
-  }
-};
-function prevSlide(e){
-  moveToSlide(currentSlide+-1);
-  let code = e.keyCode;
-  if( code == 37 ) { //left arrow key
-    moveToSlide(currentSlide+-1);
-  }
-};
-
-const slideHandlers = {
-  nextSlide: function(element){
-    document.querySelector(element).addEventListener('click',nextSlide);
-    document.body.addEventListener('keydown',nextSlide, false); // if a key is pressed, attach a listener to find out which one
-  },
-  prevSlide: function(element){
-    document.querySelector(element).addEventListener('click',prevSlide);
-    document.body.addEventListener('keydown',prevSlide, false);
-  }
+  display(index);
 }
 
-slideHandlers.nextSlide("#next");
-slideHandlers.prevSlide("#previous");
+function prevSlide() {
+  index--;
+  if(index < 0) {
+    index = slides.length - 1;
+  }
+  display(index);
+}
 
-Got this from https://cheewebdevelopment.com/boilerplate-vanilla-javascript-content-slider/ */
+next.addEventListener("click", nextSlide);
+prev.addEventListener("click", prevSlide);
+
+
+
+
+
+/* 33 minutes https://www.youtube.com/watch?v=fZlPJxq0kbk */
